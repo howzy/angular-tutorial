0b68e7e156eb28a9934644e423c3ce7ceb4382f0
howzy <ihowzy@gmail.com>
2017-08-31 23:36:45 +0800

    修复'rxjs/observable/of'报错警告

--- src/app/reactive-forms-demo/services/hero.service.ts
+++ src/app/reactive-forms-demo/services/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/Observable/of';
+import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/delay';
 
 import { Hero, heroes } from '../../data-model';
